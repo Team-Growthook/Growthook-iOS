bdf29bbc790516e2ad25caf2d46ff2bf4cf00ac7
kwonjeong <kwonjeong9470@gmail.com>
2023-11-09 18:26:09 +0900

    [Setting] #1 - 컬러 초기세팅

--- Growthook/Growthook/Global/Literals/ColorLiterals.swift
+++ Growthook/Growthook/Global/Literals/ColorLiterals.swift
@@ -9,4 +9,75 @@ import UIKit
 
 extension UIColor {
     
+    // MARK: - Green
+    
+    static var Green600: UIColor {
+        return UIColor(hex: "#0B663E")
+    }
+    
+    static var Green400: UIColor {
+        return UIColor(hex: "#23B877")
+    }
+    
+    static var Green200: UIColor {
+        return UIColor(hex: "#59D7A0")
+    }
+    
+    static var Green100: UIColor {
+        return UIColor(hex: "#9BE6C5")
+    }
+    
+    // MARK: - Red
+    
+    static var Red400: UIColor {
+        return UIColor(hex: "#FF4F17")
+    }
+    
+    static var Red200: UIColor {
+        return UIColor(hex: "#FF7E55")
+    }
+    
+    // MARK: - Gray
+    
+    static var Gray900: UIColor {
+        return UIColor(hex: "#15171E")
+    }
+    
+    static var Gray700: UIColor {
+        return UIColor(hex: "#1D1F29")
+    }
+    
+    static var Gray600: UIColor {
+        return UIColor(hex: "#292C3C")
+    }
+    
+    static var Gray500: UIColor {
+        return UIColor(hex: "#36394F")
+    }
+    
+    static var Gray400: UIColor {
+        return UIColor(hex: "#4A4D63")
+    }
+    
+    static var Gray300: UIColor {
+        return UIColor(hex: "#6B6E82")
+    }
+    
+    static var Gray200: UIColor {
+        return UIColor(hex: "#9093A8")
+    }
+    
+    static var Gray100: UIColor {
+        return UIColor(hex: "#C3C4CE")
+    }
+    
+    // MARK: - Black and White
+    
+    static var Black000: UIColor {
+        return UIColor(hex: "#151515")
+    }
+    
+    static var White000: UIColor {
+        return UIColor(hex: "#FFFFFF")
+    }
 }
