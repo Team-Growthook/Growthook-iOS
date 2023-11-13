45fab43025f73415f962fb1f39ead362dd5548d3
Cheon, Seongwoo <cjs1399@naver.com>
2023-11-13 22:35:51 +0900

    [Feature] #8 - add ActionListSegment View

---
+++ Growthook/Growthook/Presentation/ActionList/Main/View/ActionListSegmentedView.swift
@@ -0,0 +1,108 @@
+//
+//  ActionListSegmentedView.swift
+//  Growthook
+//
+//  Created by 천성우 on 11/13/23.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class ActionListSegmentedView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let inProgressButton = UIButton()
+    private let completedButtom = UIButton()
+    private let backLineView = UIView()
+    private let segmentedLineView = UIView()
+    
+    // MARK: - Properties
+    
+    private var selectedIndex: Int = 0
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+        setAddTarget()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ActionListSegmentedView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI(){
+        inProgressButton.do {
+            $0.setTitle("진행중", for: .normal)
+            $0.titleLabel?.font = .fontGuide(.body1_bold)
+            $0.setTitleColor(selectedIndex == 0 ? .green400 : .gray400, for: .normal)
+        }
+        
+        completedButtom.do {
+            $0.setTitle("완료", for: .normal)
+            $0.titleLabel?.font = .fontGuide(.body1_bold)
+            $0.setTitleColor(selectedIndex == 0 ? .gray400 : .green400, for: .normal)
+        }
+        
+        backLineView.do {
+            $0.backgroundColor = .gray400
+        }
+        
+        segmentedLineView.do {
+            $0.backgroundColor = .green400
+        }
+    }
+    
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        self.addSubviews(inProgressButton, completedButtom, backLineView, segmentedLineView)
+        
+        inProgressButton.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(2)
+            $0.height.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        completedButtom.snp.makeConstraints {
+            $0.leading.equalTo(inProgressButton.snp.trailing)
+            $0.width.equalToSuperview().dividedBy(2)
+            $0.height.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        backLineView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.bottom.equalToSuperview().inset(0.5)
+        }
+        
+        segmentedLineView.snp.makeConstraints {
+            $0.width.equalToSuperview().dividedBy(2)
+            $0.height.equalTo(2)
+            $0.bottom.equalToSuperview()
+            $0.leading.equalTo(inProgressButton.snp.leading).inset(1)
+        }
+        
+    }
+    
+    // MARK: - Methods
+    
+    private func setAddTarget() {}
+    
+    // MARK: - @objc Methods
+    
+}
