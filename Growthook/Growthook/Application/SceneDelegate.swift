cd73ddc8281749b4245704ef7a2d6feafcae6ed4
PecanPie <hidra0321@naver.com>
2023-11-14 15:52:03 +0900

    Added deviceHeight and width to replace UIScreen.main

--- Growthook/Growthook/Application/SceneDelegate.swift
+++ Growthook/Growthook/Application/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-//        let naviVC = UINavigationController(rootViewController: TabBarController())
         let splashViewController = SplashViewController()
         
         window?.rootViewController = splashViewController
