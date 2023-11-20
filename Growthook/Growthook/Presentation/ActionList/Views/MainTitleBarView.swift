0db3b865149dfb54efbcda33549c0c8b1052d6fd
Cheon, Seongwoo <cjs1399@naver.com>
2023-11-20 21:56:53 +0900

    [Feature] #8 - add PersentLabel

--- Growthook/Growthook/Presentation/ActionList/Views/MainTitleBarView.swift
+++ Growthook/Growthook/Presentation/ActionList/Views/MainTitleBarView.swift
@@ -15,6 +15,9 @@ final class MainTitleBarView: BaseView {
     // MARK: - UI Components
     
     let mainTitleLabel = UILabel()
+    let percentView = UIView()
+    let leepImage = UIImageView()
+    let percentLabel = UILabel()
     
     // MARK: - Properties
     
@@ -37,17 +40,51 @@ final class MainTitleBarView: BaseView {
             $0.font = .fontGuide(.head1)
             $0.textColor = .white000
         }
+        
+        percentView.do {
+            $0.backgroundColor = .green600
+            $0.layer.cornerRadius = 6
+        }
+        
+        leepImage.do {
+            $0.image = ImageLiterals.Component.icn_seed_light
+        }
+        
+        percentLabel.do {
+            $0.font = .fontGuide(.detail1_bold)
+            $0.textColor = .green100
+        }
     }
-    
     
     // MARK: - Layout Helper
     
     override func setLayout() {
-        addSubviews(mainTitleLabel)
+        addSubviews(mainTitleLabel, percentView)
+        percentView.addSubviews(leepImage, percentLabel)
+        
+        leepImage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(5)
+            $0.leading.equalToSuperview().offset(8)
+            $0.width.equalTo(19)
+            $0.height.equalTo(18)
+        }
+        
+        percentLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(8)
+            $0.width.equalTo(58)
+        }
         
         mainTitleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(24)
             $0.leading.equalToSuperview().offset(18)
+        }
+        
+        percentView.snp.makeConstraints {
+            $0.top.equalTo(mainTitleLabel.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(18)
+            $0.width.equalTo(97)
+            $0.height.equalTo(28)
         }
     }
     
@@ -58,6 +95,10 @@ final class MainTitleBarView: BaseView {
     func setTitleText(_ text: String) {
          mainTitleLabel.text = "\(text)님의 액션"
      }
+    
+    func setPersentText(_ text: String) {
+        percentLabel.text = "\(text)% 달성!"
+    }
     
     // MARK: - @objc Methods
     
