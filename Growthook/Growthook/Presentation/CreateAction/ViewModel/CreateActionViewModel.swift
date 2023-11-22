2b71c6fea6b92696c485be8bc3eb1d2f379d98a6
frohsch <kminjoo1213@gmail.com>
2023-11-23 00:11:38 +0900

    [Fix] #16 - ViewModel 수정

--- Growthook/Growthook/Presentation/CreateAction/ViewModel/CreateActionViewModel.swift
+++ Growthook/Growthook/Presentation/CreateAction/ViewModel/CreateActionViewModel.swift
@@ -11,12 +11,6 @@ import RxCocoa
 import RxSwift
 import RxRelay
 
-enum FoldStatus {
-    case folded
-    case unfolded
-    case none
-}
-
 protocol CreateActionViewModelInputs {
     func setActionPlan(with value: String)
     func setFolded(with value: FoldStatus)
@@ -26,7 +20,6 @@ protocol CreateActionViewModelOutputs {
     var name: BehaviorRelay<String> { get }
     var insight: BehaviorRelay<InsightModel> { get }
     var action: BehaviorRelay<String> { get }
-    var folded: BehaviorRelay<FoldStatus> { get }
 }
 
 protocol CreateActionViewModelType {
@@ -39,27 +32,13 @@ final class CreateActionViewModel: CreateActionViewModelInputs, CreateActionView
     func setActionPlan(with value: String) {
         action.accept(value)
     }
-    
-    func setFolded(with value: FoldStatus) {
-        folded.accept(value)
-        switch folded.value {
-        case .folded:
-            folded.accept(.unfolded)
-        case .unfolded:
-            folded.accept(.folded)
-        case .none:
-            break
-        }
-        folded.accept(.none)
-    }
-    
+
     var inputs: CreateActionViewModelInputs { return self }
     var outputs: CreateActionViewModelOutputs { return self }
     
     var name = BehaviorRelay<String>(value: "")
     var insight = BehaviorRelay<InsightModel>(value: InsightModel(name: "", insight: "", date: "", dDay: "", memo: ""))
     var action = BehaviorRelay<String>(value: "")
-    var folded = BehaviorRelay<FoldStatus>(value: .none)
     
     init() {
         insight.accept(InsightModel.dummy())
