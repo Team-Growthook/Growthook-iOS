fa07b5ca732bdf89846fcb0c0115401f671aa885
frohsch <kminjoo1213@gmail.com>
2023-11-22 14:15:44 +0900

    [Feat] #16 - 액션플랜 생성 ViewController 구현

--- Growthook/Growthook/Presentation/CreateAction/ViewController/CreateActionViewControlller.swift
+++ Growthook/Growthook/Presentation/CreateAction/ViewController/CreateActionViewControlller.swift
@@ -1,8 +1,43 @@
 //
-//  CreateViewControlller.swift
+//  CreateActionViewControlller.swift
 //  Growthook
 //
 //  Created by Minjoo Kim on 11/22/23.
 //
 
-import Foundation
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import Then
+
+final class CreateActionViewControlller: BaseViewController {
+    
+    private let createActionView = CreateActionView()
+    private var viewModel = CreateActionViewModel()
+    private let disposeBag = DisposeBag()
+    
+    override func loadView() {
+        self.view = createActionView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    deinit {
+        print(#function)
+    }
+    
+//    override func bindViewModel() {
+//        <#code#>
+//    }
+}
