305e28f4eb5cf4391d159cb3c67bec68f7c2947b
Cheon, Seongwoo <cjs1399@naver.com>
2023-11-29 12:59:46 +0900

    [Feature] #8 - 완료하기 테이블 VC 작성

--- Growthook/Growthook/Presentation/ActionList/Controllers/CompleteViewController.swift
+++ Growthook/Growthook/Presentation/ActionList/Controllers/CompleteViewController.swift
@@ -20,7 +20,12 @@ final class CompleteViewController: BaseViewController {
     
     // MARK: - UI Components
     
+    private let scrapButton = ScrapOnlyButton()
+    private let tableView = UITableView(frame: .zero, style: .plain)
+    
     // MARK: - Properties
+    
+    weak var delegate: viewMove?
     
     // MARK: - Initializer
     
@@ -31,22 +36,74 @@ final class CompleteViewController: BaseViewController {
     }
     
     override func bindViewModel() {
-
+        
     }
     
     // MARK: - UI Components Property
     
     override func setStyles() {
-        view.backgroundColor = .green400
+        view.backgroundColor = .gray700
+        
+        tableView.do {
+            $0.showsVerticalScrollIndicator = false
+            $0.separatorStyle = .none
+            $0.register(ActionListCompleteTableViewCell.self, forCellReuseIdentifier: "completeActionListTableCell")
+            $0.delegate = self
+            $0.dataSource = self
+            $0.backgroundColor = .gray700
+        }
+        
     }
     
     // MARK: - Layout Helper
     
     override func setLayout() {
-
+        view.addSubviews(scrapButton, tableView)
+        
+        scrapButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(13)
+            $0.trailing.equalToSuperview().inset(18)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(scrapButton.snp.bottom).offset(13)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
     
     // MARK: - Methods
     
     // MARK: - @objc Methods
+    
+    @objc func buttonTapped() {
+        delegate?.didTapButtonInCompleteViewController()
+    }
+}
+
+
+extension CompleteViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 111
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.outputs.completeActionList.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "completeActionListTableCell", for: indexPath) as! ActionListCompleteTableViewCell
+        let model = viewModel.outputs.completeActionList.value[indexPath.row]
+        cell.configure(model)
+        
+        cell.reviewButton.rx.tap
+            .bind { [weak self]  in
+                guard let self else { return }
+                self.viewModel.inputs.didTapReviewButton()
+                self.buttonTapped()
+            }
+            .disposed(by: disposeBag)
+        
+        return cell
+    }
 }
