70e5c30719cce121a01fd545ff8b4b2d70ff584d
kwonjeong <kwonjeong9470@gmail.com>
2023-12-11 18:39:47 +0900

    [Feat] #22 - 카카오 로그인 구현

--- Growthook/Growthook/Application/SceneDelegate.swift
+++ Growthook/Growthook/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let splashViewController = TabBarController()
+        let splashViewController = KakaoLoginViewController()
         
         window?.rootViewController = splashViewController
         window?.makeKeyAndVisible()
