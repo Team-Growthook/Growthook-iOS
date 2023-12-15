fee2a670ec86a8dee03695450bffef7633ce4b93
PecanPie <hidra0321@naver.com>
2023-12-16 02:10:03 +0900

    [Feat] - #25 View 에 필요한 하위 View 들을 추가했습니다.
    1. 쑥 개수를 나타내는 뷰, 2. 마이페이지 테이블뷰 cell

--- Growthook/Growthook/Presentation/Mypage/Views/Components/MyPageListTableViewCell.swift
+++ Growthook/Growthook/Presentation/Mypage/Views/Components/MyPageListTableViewCell.swift
@@ -7,17 +7,50 @@
 
 import UIKit
 
-class MyPageListTableViewCell: UITableViewCell {
+final class MyPageListTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private let mainTitleLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setStyles()
+        setLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+}
 
+extension MyPageListTableViewCell {
+    
+    private func setStyles() {
+        backgroundColor = .gray600
+        selectionStyle = .none
+        
+        mainTitleLabel.do {
+            $0.font = .fontGuide(.body2_bold)
+            $0.textColor = .white000
+        }
+    }
+    
+    private func setLayout() {
+        addSubviews(mainTitleLabel)
+        
+        mainTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(18)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
+
+extension MyPageListTableViewCell {
+    
+    func configureWith(componentTitle: String) {
+        mainTitleLabel.text = componentTitle
+    }
+    
+    func setLogOutStyle() {
+        mainTitleLabel.textColor = .red400
+    }
 }
