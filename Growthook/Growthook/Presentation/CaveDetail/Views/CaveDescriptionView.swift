b9a4bb57d5cff1ce3c38b6f7fb074d7068fb9a64
kwonjeong <kwonjeong9470@gmail.com>
2023-12-16 14:41:23 +0900

    [Chore] #24 - 보관함 잠금 기능 이미지 변경

---
+++ Growthook/Growthook/Presentation/CaveDetail/Views/CaveDescriptionView.swift
@@ -0,0 +1,20 @@
+//
+//  CaveDescriptionView.swift
+//  Growthook
+//
+//  Created by KJ on 12/16/23.
+//
+
+import UIKit
+
+class CaveDescriptionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
