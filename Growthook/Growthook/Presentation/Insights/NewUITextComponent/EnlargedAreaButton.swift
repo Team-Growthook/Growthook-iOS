d5612242f1b8ed4f242924abc913ec8704537394
PecanPie <hidra0321@naver.com>
2023-12-21 23:37:12 +0900

    [Feat] - #28 Touch Area 가 넓어진 CustomButton 을 추가헀습니다.

--- Growthook/Growthook/Presentation/Insights/NewUITextComponent/EnlargedAreaButton.swift
+++ Growthook/Growthook/Presentation/Insights/NewUITextComponent/EnlargedAreaButton.swift
@@ -7,14 +7,15 @@
 
 import UIKit
 
+    /// 상속 받아 사용할 수 있게, final 은 생략했습니다.
 class EnlargedAreaButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        super.point(inside: point, with: event)
+        
+        /// 모든 방향에 값 만큼 터치 영역 증가
+        /// dx: x축이 dx만큼 증가 (음수여야 증가)
+        let touchArea = bounds.insetBy(dx: -10, dy: -10)
+        return touchArea.contains(point)
     }
-    */
-
 }
