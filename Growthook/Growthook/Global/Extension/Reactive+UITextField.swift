523adb591cdff7b629386f3cf77f4b9bd4eb1420
PecanPie <hidra0321@naver.com>
2023-12-21 23:24:37 +0900

    [Feat] - #28 Reactive+UITextfield extension 을 추가해, 내부에 특정 event 에만 받는 text 값을 rx 로 뽑았습니다.

--- Growthook/Growthook/Global/Extension/Reactive+UITextField.swift
+++ Growthook/Growthook/Global/Extension/Reactive+UITextField.swift
@@ -5,4 +5,20 @@
 //  Created by KYUBO A. SHIM on 12/20/23.
 //
 
-import Foundation
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UITextField {
+    
+    var changedText: ControlProperty<String?> {
+        return base.rx.controlProperty(editingEvents: [.editingChanged, .valueChanged], getter: { textField in
+            textField.text
+        }, setter: { textField, value in
+            if textField.text != value {
+                textField.text = value
+            }
+        })
+    }
+}
