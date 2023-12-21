121aba83e856f7e5e22c5a7da2e0e03741bf4a8d
PecanPie <hidra0321@naver.com>
2023-12-21 23:38:41 +0900

    [Feat] - #28 최하단의 버튼에 직접 접근하는 것을 막기 위해, ControlEvent 를 내뱉는 Observable 을 만들어 rx 로 처리합니다.

--- Growthook/Growthook/Presentation/Insights/NewUITextComponent/CommonCustomNavigationBar.swift
+++ Growthook/Growthook/Presentation/Insights/NewUITextComponent/CommonCustomNavigationBar.swift
@@ -7,14 +7,74 @@
 
 import UIKit
 
-class CommonCustomNavigationBar: UIView {
+import RxCocoa
+import RxSwift
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class CommonCustomNavigationBar: BaseView {
+
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Extended .rx Properties for Button Event
+    lazy var rxBackButtonTapControl: ControlEvent<Void> = backButton.rx.tap
+    lazy var rxDoneButtonTapControl: ControlEvent<Void> = doneButton.rx.tap
+    
+    // MARK: - UI Properties
+    private let backButton = UIButton()
+    private let titleLabel = UILabel()
+    private let doneButton = EnlargedAreaButton()
+
+    // MARK: - Styles
+    override func setStyles() {
+        backgroundColor = .clear
+        
+        backButton.do {
+            $0.setImage(ImageLiterals.NavigationBar.back, for: .normal)
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        titleLabel.do {
+            $0.font = .fontGuide(.body1_reg)
+            $0.textColor = .white000
+        }
+        
+        doneButton.do {
+            $0.setTitle("등록", for: .normal)
+            $0.setTitleColor(.gray300, for: .normal)
+            $0.titleLabel?.font = .fontGuide(.body1_bold)
+            $0.isEnabled = false
+        }
     }
-    */
+    
+    // MARK: - Layout
+    override func setLayout() {
+        addSubviews(backButton, titleLabel, doneButton)
+        
+        backButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(8)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(48)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
 
+        doneButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(18)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(40)
+        }
+    }
+}
+
+extension CommonCustomNavigationBar {
+    
+    // MARK: - Methods for Configuring View itself.
+    func setTitle(with title: String) {
+        titleLabel.text = title
+    }
+    
+    func isButtonEnabled(_ isEnabled: Bool) {
+        doneButton.isEnabled = isEnabled
+    }
 }
