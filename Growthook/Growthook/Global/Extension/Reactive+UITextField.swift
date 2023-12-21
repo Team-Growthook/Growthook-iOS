f3425b93772c62b9af4442643bdcef243f9d045a
PecanPie <hidra0321@naver.com>
2023-12-21 23:25:41 +0900

    [Docs] - #28 extension 내용에 대해서 주석을 추가했습니다.

--- Growthook/Growthook/Global/Extension/Reactive+UITextField.swift
+++ Growthook/Growthook/Global/Extension/Reactive+UITextField.swift
@@ -12,6 +12,10 @@ import RxSwift
 
 extension Reactive where Base: UITextField {
     
+    // MARK: 만약, 모듈화를 진행한다면 public 을 추가해야합니다.
+    /**
+     UITextField 에서 .editingChanged, .valueChanged 의 상태에서 변경된 text 만을 사용합니다.
+     */
     var changedText: ControlProperty<String?> {
         return base.rx.controlProperty(editingEvents: [.editingChanged, .valueChanged], getter: { textField in
             textField.text
