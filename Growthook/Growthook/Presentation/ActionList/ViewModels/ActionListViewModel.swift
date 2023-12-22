7c8ded1cb7df851d00eea8db740b2147c5dd4bad
Cheon, Seongwoo <cjs1399@naver.com>
2023-12-22 14:27:30 +0900

    [Feature] #19 - add BottomSheet CancelButton Input ViewModel

--- Growthook/Growthook/Presentation/ActionList/ViewModels/ActionListViewModel.swift
+++ Growthook/Growthook/Presentation/ActionList/ViewModels/ActionListViewModel.swift
@@ -16,6 +16,7 @@ protocol ActionListViewModelInput {
     func didTapCompletButton()
     func didTapReviewButton()
     func setReviewText(with value: String)
+    func didTapCancelButtonInBottomSheet()
 }
 
 protocol ActionListViewModelOutput {
@@ -39,7 +40,7 @@ final class ActionListViewModel: ActionListViewModelInput, ActionListViewModelOu
     var actionList: BehaviorRelay<[ActionListModel]> = BehaviorRelay(value: [])
     var completeActionList: BehaviorRelay<[CompleteActionListModel]> = BehaviorRelay(value: [])
     var reviewText = BehaviorRelay<String>(value: "")
-    
+
     var inputs: ActionListViewModelInput { return self }
     var outputs: ActionListViewModelOutput { return self }
     
@@ -53,14 +54,18 @@ final class ActionListViewModel: ActionListViewModelInput, ActionListViewModelOu
     }
     
     var isReviewEntered: Driver<Bool> {
-         return reviewText.asDriver()
-             .map { !$0.isEmpty }
-     }
+        return reviewText.asDriver()
+            .map { text in
+                return !(text.isEmpty || text == "액션 플랜을 달성하며 어떤 것을 느꼈는지 작성해보세요")
+            }
+    }
+    
     
     var reviewTextCount: Driver<String> {
         return reviewText.asDriver()
             .map { "\($0.count)/300" }
     }
+    
     
     init() {
         self.actionList.accept(ActionListModel.actionListModelDummyData())
@@ -70,7 +75,7 @@ final class ActionListViewModel: ActionListViewModelInput, ActionListViewModelOu
     func didTapInProgressButton() {
         selectedIndex.accept(1)
     }
-
+    
     func didTapCompletedButton() {
         selectedIndex.accept(0)
     }
@@ -94,16 +99,9 @@ final class ActionListViewModel: ActionListViewModelInput, ActionListViewModelOu
     func setReviewText(with value: String) {
         reviewText.accept(value)
     }
+    
+    func didTapCancelButtonInBottomSheet() {
+        didTapCompletedButton()
+    }
+    
 }
-
-/**
- Input은
- 1. 사용자의 이벤트 입력
- 2. 뷰 컨트롤러 사이클에 의 한 입력
- 
- 액션리스트 화면에서 사용자의 입력은
- 진행중/완료 탭 터치
- 스크랩만 보기 버튼 터치
- 씨앗 보기 버튼 터치
- 
- */
