c99ed1a6e310c49b8173fa703df3426a88132df8
kwonjeong <kwonjeong9470@gmail.com>
2023-12-23 21:51:35 +0900

    [Fix] #34 - 인사이트 리스트 모델 수정

--- Growthook/Growthook/Presentation/Home/Models/InsightList.swift
+++ Growthook/Growthook/Presentation/Home/Models/InsightList.swift
@@ -8,15 +8,14 @@
 import UIKit
 
 enum InsightStatus {
-    case scrapLight
     case light
-    case lock
-    case scrapDark
     case dark
+    case lock
 }
 
 struct InsightList {
-    let scrapStatus: InsightStatus
+    let InsightStatus: InsightStatus
+    let scrapStatus: Bool
     let title: String
     let dueTime: String
 }
@@ -25,27 +24,17 @@ extension InsightList {
     
     static func insightListDummyData() -> [InsightList] {
         return [
-            InsightList(scrapStatus: .lock, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금"),
-            InsightList(scrapStatus: .scrapLight, title: "쑥쑦이들은 최고다.", dueTime: "00일 후 잠금")
+            InsightList(InsightStatus: .light, scrapStatus: true, title: "안녕하세요", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .light, scrapStatus: false, title: "쑥쑥이들은 최고다.", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .light, scrapStatus: false, title: "iOS 짱이당", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .light, scrapStatus: false, title: "민주 화이팅", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .light, scrapStatus: false, title: "성우오빠 화이팅", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .lock, scrapStatus: false, title: "규보오빠 화이팅", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .dark, scrapStatus: true, title: "권정 화이팅", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .lock, scrapStatus: false, title: "개발 언제끝남? ㅋㅋ", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .dark, scrapStatus: true, title: "진짜 언제 끝남?", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .dark, scrapStatus: false, title: "왜 안끝남?", dueTime: "00일 후 잠금"),
+            InsightList(InsightStatus: .dark, scrapStatus: true, title: "서버는 붙이는거 무섭다..", dueTime: "00일 후 잠금")
         ]
     }
 }
