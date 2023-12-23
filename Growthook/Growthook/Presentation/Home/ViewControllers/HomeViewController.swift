b7b1c45dc92077dd5c37128f27fee1de66d6443e
kwonjeong <kwonjeong9470@gmail.com>
2023-12-23 21:32:00 +0900

    [Chore] #34 - 인사이트 컬렉션뷰 코드 정리

--- Growthook/Growthook/Presentation/Home/ViewControllers/HomeViewController.swift
+++ Growthook/Growthook/Presentation/Home/ViewControllers/HomeViewController.swift
@@ -61,18 +61,27 @@ final class HomeViewController: BaseViewController {
             }
                        .disposed(by: disposeBag)
         
-//        viewModel.outputs.insightList
-//            .bind(to: insightListView.insightCollectionView.rx
-//                .items(cellIdentifier: InsightListCollectionViewCell.className,
-//                       cellType: InsightListCollectionViewCell.self)) { (index, model, cell) in
-//                cell.configureCell(model)
-//                cell.setCellStyle()
-//                cell.scrapButtonTapHandler = { [weak self] in
-//                    cell.scrapButtonTapped()
-//                    cell.setCellStyle()
-//                }
-//            }
-//                       .disposed(by: disposeBag)
+        viewModel.outputs.insightList
+            .bind(to: insightListView.insightCollectionView.rx
+                .items(cellIdentifier: InsightListCollectionViewCell.className,
+                       cellType: InsightListCollectionViewCell.self)) { (index, model, cell) in
+                cell.configureCell(model)
+                cell.setCellStyle()
+                cell.scrapButtonTapHandler = { [weak self] in
+                    cell.scrapButtonTapped()
+                    cell.setCellStyle()
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        // 인사이트 셀 스타일 재설정
+        insightListView.insightCollectionView.rx.willDisplayCell
+            .subscribe(onNext: { event in
+                if let cell = event.cell as? InsightListCollectionViewCell {
+                    cell.setCellStyle()
+                }
+            })
+            .disposed(by: disposeBag)
         
         viewModel.outputs.insightLongTap
             .subscribe(onNext: { [weak self] indexPath in
--- Growthook/Growthook/Presentation/Home/Views/InsightListView.swift
+++ Growthook/Growthook/Presentation/Home/Views/InsightListView.swift
@@ -26,16 +26,6 @@ final class InsightListView: BaseView {
     var scrapType: Bool = false
     private let dummyModel: [InsightList] = InsightList.insightListDummyData()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        insightCollectionView.delegate = self
-        insightCollectionView.dataSource = self
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     // MARK: - UI Components Property
     
     override func setStyles() {
@@ -90,36 +80,5 @@ final class InsightListView: BaseView {
             $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
-    }
-}
-
-extension InsightListView: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if let cell = cell as? InsightListCollectionViewCell {
-            cell.setCellStyle()
-//            cell.configureCell(dummyModel[indexPath.row])
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if let cell = cell as? InsightListCollectionViewCell {
-            cell.setCellStyle()
-//            cell.configureCell(dummyModel[indexPath.row])
-        }
-    }
-}
-
-extension InsightListView: UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dummyModel.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueCell(type: InsightListCollectionViewCell.self, indexPath: indexPath)
-        cell.configureCell(dummyModel[indexPath.row])
-        return cell
-        
     }
 }
