5fc0ac56313cec565b5b9d44c9583586a249a573
kwonjeong <kwonjeong9470@gmail.com>
2023-12-23 16:39:25 +0900

    [Fix] #34 - 토스트 메시지 텍스트 변경

--- Growthook/Growthook/Presentation/Home/ViewControllers/HomeViewController.swift
+++ Growthook/Growthook/Presentation/Home/ViewControllers/HomeViewController.swift
@@ -321,9 +321,9 @@ extension HomeViewController {
         if let info = notification.userInfo?["type"] as? ClearInsightType {
             switch info {
             case .move:
-                view.showToast(message: "옮김")
+                view.showToast(message: "씨앗을 옮겨 심었어요")
             case .delete:
-                view.showToast(message: "삭제")
+                view.showToast(message: "씨앗이 삭제되었어요")
             }
         }
     }
