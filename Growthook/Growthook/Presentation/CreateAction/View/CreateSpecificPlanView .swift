77e23e3482ce3a6efc28cc6d2293ecf6434e8516
frohsch <kminjoo1213@gmail.com>
2023-12-24 10:13:19 +0900

    [Feat] #29 - Modify PlanView

--- Growthook/Growthook/Presentation/CreateAction/View/CreateSpecificPlanView .swift
+++ Growthook/Growthook/Presentation/CreateAction/View/CreateSpecificPlanView .swift
@@ -13,24 +13,30 @@ import Then
 final class CreateSpecificPlanView: BaseView {
     
     let plusButton = UIButton()
-    let planCollectionView = UICollectionView()
+    let planCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     override func setStyles() {
         plusButton.setImage(ImageLiterals.ActionPlan.btn_plus, for: .normal)
+        
+        planCollectionView.do {
+            $0.backgroundColor = .clear
+            $0.contentInsetAdjustmentBehavior = .never
+            $0.showsVerticalScrollIndicator = false
+        }
     }
     
     override func setLayout() {
         self.addSubviews(plusButton, planCollectionView)
         
         plusButton.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(12)
+            $0.top.equalToSuperview()
             $0.trailing.equalToSuperview().inset(26)
         }
         
         planCollectionView.snp.makeConstraints {
             $0.top.equalTo(plusButton.snp.bottom).offset(7)
             $0.horizontalEdges.equalToSuperview().inset(18)
-            $0.bottom.equalTo(self.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
         }
     }
 }
