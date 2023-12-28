9461aac1098076114cdbae283897f058ac742a71
kwonjeong <kwonjeong9470@gmail.com>
2023-12-28 15:58:02 +0900

    [Setting] #42 - Encodable 익스텐션 추가

---
+++ Growthook/Growthook/Global/Extension/Encodable+.swift
@@ -0,0 +1,23 @@
+//
+//  Encodable+.swift
+//  Growthook
+//
+//  Created by KJ on 12/28/23.
+//
+
+import Foundation
+
+// MARK: - Encodable Extension
+
+extension Encodable {
+    
+  func asParameter() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            as? [String: Any] else {
+      throw NSError()
+    }
+    
+    return dictionary
+  }
+}
