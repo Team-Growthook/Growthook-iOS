345a71cca0f2c94475d3df183af75cc23cf6ed61
PecanPie <hidra0321@naver.com>
2024-01-02 23:56:47 +0900

    [Fix] - #45 확정은 아니지만, 서비스 내에서 생길 수 있는 커스텀 에러 설정

--- Growthook/Growthook/Network/ServiceError/ServiceError.swift
+++ Growthook/Growthook/Network/ServiceError/ServiceError.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+import Moya
+
+enum ServiceError: Error {
+    case moyaError(MoyaError)
+    case invalidResponse(responseCode: Int, message: String)
+    case tokenExpired
+    case refreshTokenExpired
+}
+
+extension ServiceError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .moyaError(let moyaError):
+            return "❗️" + moyaError.localizedDescription + "❗️"
+        case .invalidResponse(_, let message):
+            return "❗️" + message + "❗️"
+        case .tokenExpired:
+            return "❗️Access Token Expired❗️"
+        case .refreshTokenExpired:
+            return "❗️Refresh Token Expired❗️"
+        }
+    }
+}
