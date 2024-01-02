f0ce32e007529406d99345fe78cbb5eab0ab9fcb
PecanPie <hidra0321@naver.com>
2024-01-02 23:55:03 +0900

    [Fix] - #45 Custom Provider 를 만들수 있는 인터페이스 구현

--- Growthook/Growthook/Network/Base/Networkable.swift
+++ Growthook/Growthook/Network/Base/Networkable.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol Networkable {
+    associatedtype Target: TargetType
+    
+    static func makeProvider() -> MoyaProvider<Target>
+}
+
+extension Networkable {
+    
+    static func makeProvider() -> MoyaProvider<Target> {
+        let authPlugin = AccessTokenPlugin { _ in
+            return "🔐 With Authorization"
+        }
+        let loggerPlugin = NetworkLoggerPlugin()
+        
+        return MoyaProvider<Target>(plugins: [loggerPlugin])
+    }
+}
